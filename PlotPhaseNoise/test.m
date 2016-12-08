137ae6e6ee814dd45579e5db9b9230776ab86983
Yining <zyndct@gmail.com>
2016-12-08 17:58:33 +0800

    Update test.m

--- PlotPhaseNoise/test.m
+++ PlotPhaseNoise/test.m
@@ -1,4 +1,8 @@
 
 % [t_value sig] = textread('OUT_SP.dat'); % uncomment this sentence to read-in signal from file
 
-PlotPhaseNoise(sig, 1e10, 1, 100e6, -160, -20);
+[mag, angle] = PlotPhaseNoise(sig, 1e10, 1, 100e6, -160, -20);
+
+plot(20*log10(mag));
+ylim([-100, 50]); % since we have removed DC of signal, the fft plot will give you an extremely
+                  % small number on mag(1). Remember to take care of it.

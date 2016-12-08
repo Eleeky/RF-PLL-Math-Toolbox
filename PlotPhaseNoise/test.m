2a434ef00c730f17843ab21a2dddc92d3ce0e131
Yining <zyndct@gmail.com>
2016-12-08 18:05:12 +0800

    Update test.m

--- PlotPhaseNoise/test.m
+++ PlotPhaseNoise/test.m
@@ -1,7 +1,10 @@
 
 % [t_value sig] = textread('OUT_SP.dat'); % uncomment this sentence to read-in signal from file
 
-[mag, angle] = PlotPhaseNoise(sig, 1e10, 1, 100e6, -160, -20);
+% add window to signal
+sigNew = sig .* hann(length(sig));
+
+[mag, angle] = PlotPhaseNoise(sigNew, 1e10, 1, 100e6, -160, -20);
 
 plot(20*log10(mag));
 ylim([-100, 50]); % since we have removed DC of signal, the fft plot will give you an extremely
